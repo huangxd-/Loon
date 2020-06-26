3928acc3e1011e34bf8f01820cd815d3787fad8b
root <649924757@qq.com>
2020-06-27 06:10:08 +0800

    添加chordify.js

---
+++ JS/chordify.js
@@ -0,0 +1,11 @@
+/*
+chordify app unlocks premium
+Surge4.0:
+http-response https:\/\/chordify\.net\/api\/v1\/users\/me requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/VUE.js
+QX 1.0.0:
+^https:\/\/chordify\.net\/api\/v1\/users\/me url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/VUE.js
+Surge & QX Mitm = chordify.net
+*/
+
+body = $response.body.replace(/\"member_type\":free/, "\"member_type\":premium")
+$done({body});
