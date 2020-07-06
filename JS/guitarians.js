989d178530222915bd4f28c97783688ef916741d
huangxd- <649924757@qq.com>
2020-07-06 23:22:21 +0800

    Update guitarians.js

--- JS/guitarians.js
+++ JS/guitarians.js
@@ -3,7 +3,4 @@ guitarians app unlocks pro
 */
 
 body = $response.body.replace(/\"member_type\": 0/, "\"member_type\": 2")
-body = $response.body.replace(/\"member_type\":0/, "\"member_type\":2")
-body = $response.body.replace(/\"member_type\": 0, "\"member_type\": 2")
-body = $response.body.replace(/\"member_type\":0, "\"member_type\":2")
 $done({body});
