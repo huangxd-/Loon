eff027e345ea9817b346235bac7f1fbe30fb19f8
huangxd- <649924757@qq.com>
2020-07-07 22:26:11 +0800

    Update guitarians.js

--- JS/guitarians.js
+++ JS/guitarians.js
@@ -2,5 +2,7 @@
 guitarians app unlocks pro
 */
 
+$notification.post('title', 'subtitle', response.body)
 body = $response.body.replace(/\"member_type\": 0/, "\"member_type\": 2").replace(/\"ad_threshold\": 10/, "\"ad_threshold\": 0")
+$notification.post('title', 'subtitle', {body})
 $done({body});
