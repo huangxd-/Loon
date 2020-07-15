11f3a91c00d4fc0829f4b989fdb84520724ee2a0
huangxd- <649924757@qq.com>
2020-07-15 22:52:38 +0800

    Update xunlei.js

--- JS/xunlei.js
+++ JS/xunlei.js
@@ -3,5 +3,5 @@ xunlei app unlocks premium
 */
 
 body = $response.body.replace(/\"isVip\":\"0\"/, "\"isVip\":\"1\"").replace(/\"isYear\":\"0\"/, "\"isYear\":\"1\"").replace(/\"payId\":\"0\"/, "\"payId\":\"5\"").replace(/\"vipLevel\":\"0\"/, "\"vipLevel\":\"9\"")
-#$notification.post($request.url, $request.method, body)
+// $notification.post($request.url, $request.method, body)
 $done({body});
