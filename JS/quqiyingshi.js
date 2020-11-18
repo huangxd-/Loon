93155863f8a5346f08d6aea61bfb514eeb3f6a47
huangxd- <649924757@qq.com>
2020-11-18 21:49:21 +0800

    Delete quqiyingshi.js

--- JS/quqiyingshi.js
+++
@@ -1,7 +0,0 @@
-/*
-quqiyingshi app unlocks vip
-*/
-
-body = $response.body.replace(/\"isVip\":\"0\"/, "\"isVip\":\"1\"").replace(/\"vipLevel\":\"0\"/, "\"vipLevel\":\"9\"")
-$notification.post($request.url, $request.method, body)
-$done({body});
