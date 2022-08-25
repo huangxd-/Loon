1cb2d6f7ffe146dc73d14bbe147aaac522f348eb
huangxd- <649924757@qq.com>
2022-08-25 14:37:24 +0800

    Update chongchongguitar.js

--- JS/chongchongguitar.js
+++ JS/chongchongguitar.js
@@ -2,5 +2,5 @@
 chognchong guitar app unlocks pro
 */
 body = $response.body.replace(/\"is_vip\":\"0\"/, "\"is_vip\":\"1\"").replace(/\"expire_date\":\"\"/, "\"expire_date\":\"2030-11-11\"").replace(/\"has_buy\":\"0\"/, "\"has_buy\":\"1\"")
-$notification.post($request.method, body)
+// $notification.post($request.url, $request.method, body)
 $done({body});
